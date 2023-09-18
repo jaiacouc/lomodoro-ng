4a27531c08101c8df0c7a982592f99576a8831de
John <jaiacouc@uncg.edu>
2023-09-18 16:41:46 -0400

    Added about page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'pomodoro', component: PomodoroComponent },
   { path: '', component: PomodoroComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
