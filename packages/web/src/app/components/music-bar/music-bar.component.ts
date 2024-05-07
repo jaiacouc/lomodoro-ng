241c0d3abdd4211014f66e5827174da4f9c241b6
John <jaiacouc@uncg.edu>
2024-05-07 09:22:59 -0400

    Created music bar

---
+++ packages/web/src/app/components/music-bar/music-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-bar.component.html',
+  styleUrl: './music-bar.component.scss'
+})
+export class MusicBarComponent {
+
+}
