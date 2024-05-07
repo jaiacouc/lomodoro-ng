7ed98279cb8f7c50fbeb557dee54c6d52f745ce2
John <jaiacouc@uncg.edu>
2024-05-07 08:57:16 -0400

    Updated Timer to fix multiple start bug. Added the ability to resume a timer

--- packages/web/src/app/components/timer/timer.component.ts
+++ packages/web/src/app/components/timer/timer.component.ts
@@ -17,14 +17,18 @@ import { MatCard, MatCardHeader, MatCardContent } from '@angular/material/card';
   imports: [MatCard, MatCardHeader, MatTabGroup, MatTab, SettingsComponent, MatCardContent, MatButton, NgIf],
 })
 export class TimerComponent implements OnDestroy {
-  // public workTime: number = 45;
+  private readonly MILLITOMIN: number = 60000;
+
   public workTime: WritableSignal<number> = signal(45);
   public breakTime: WritableSignal<number> = signal(15);
   public timeLeft: Signal<string> = computed(() => this.workingTime().toFormat('mm:ss'));
-  public stopped: WritableSignal<boolean> = signal(false);
+  public stopped: WritableSignal<boolean> = signal(true);
+  public disableStopButton: Signal<boolean> = computed(
+    () => this.timeLeft() === this.workTime() + ':00' || this.timeLeft() === this.breakTime() + ':00',
+  );
   public tabIndex: number = 0;
 
-  private workingTime: WritableSignal<Duration> = signal(Duration.fromMillis(this.workTime() * 60000));
+  private workingTime: WritableSignal<Duration> = signal(Duration.fromMillis(this.workTime() * this.MILLITOMIN));
   private currentTimeSelection: WritableSignal<number> = signal(this.workTime());
   private working: boolean = true;
   private interval: any;
@@ -42,15 +46,28 @@ export class TimerComponent implements OnDestroy {
 
   public startTimer(): void {
     this.stopped.set(false);
-    const timeEnd: DateTime = DateTime.now().plus({
-      minutes: this.currentTimeSelection(),
-    });
-    this.interval = setInterval(() => {
-      this.workingTime.set(timeEnd.diff(DateTime.now(), ['minutes', 'seconds']));
-      if (this.workingTime().minutes === 0 && Math.trunc(this.workingTime().seconds) === 0) {
-        this.stopTimer();
-      }
-    }, 1000);
+    if (this.workingTime().milliseconds / this.MILLITOMIN === this.currentTimeSelection()) {
+      const timeEnd: DateTime = DateTime.now().plus({
+        minutes: this.currentTimeSelection(),
+      });
+      this.interval = setInterval(() => {
+        this.workingTime.set(timeEnd.diff(DateTime.now(), ['minutes', 'seconds']));
+        if (this.workingTime().minutes === 0 && Math.trunc(this.workingTime().seconds) === 0) {
+          this.stopTimer();
+        }
+      }, 1000);
+    } else {
+      const timeEnd: DateTime = DateTime.now().plus({
+        minutes: this.workingTime().minutes,
+        seconds: this.workingTime().seconds,
+      });
+      this.interval = setInterval(() => {
+        this.workingTime.set(timeEnd.diff(DateTime.now(), ['minutes', 'seconds']));
+        if (this.workingTime().minutes === 0 && Math.trunc(this.workingTime().seconds) === 0) {
+          this.stopTimer();
+        }
+      });
+    }
   }
 
   public stopTimer(): void {
@@ -60,7 +77,7 @@ export class TimerComponent implements OnDestroy {
 
   public changeTimer(): void {
     // Stop so next button changes and disables.
-    this.stopped.set(false);
+    this.stopped.set(true);
     if (this.working) {
       this.working = false;
       this.tabIndex = 1;
@@ -86,7 +103,7 @@ export class TimerComponent implements OnDestroy {
   }
 
   private setTime(time: number) {
-    this.workingTime.set(Duration.fromMillis(time * 60000));
+    this.workingTime.set(Duration.fromMillis(time * this.MILLITOMIN));
     this.currentTimeSelection.set(time);
   }
 }
