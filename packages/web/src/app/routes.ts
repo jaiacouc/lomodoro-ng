95f913df45c3907897bcd1c056f815b632ce85bb
John <jaiacouc@uncg.edu>
2024-05-06 20:17:01 -0400

    Updated routing to be standalone

--- packages/web/src/app/routes.ts
+++ packages/web/src/app/routes.ts
@@ -1,16 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'pomodoro', component: PomodoroComponent },
   { path: '', component: PomodoroComponent },
   { path: 'about', component: AboutComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,18 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppNavModule } from './app/app-nav/app-nav.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
+import { routes } from './app/routes';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, AppNavModule),
-        provideAnimations()
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [provideAnimations(), provideRouter(routes)],
+}).catch((err) => console.error(err));
