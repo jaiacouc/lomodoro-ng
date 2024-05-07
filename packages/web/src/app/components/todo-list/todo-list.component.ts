543f7a31f924911e5b1dd34872d96901f567cfa9
John <jaiacouc@uncg.edu>
2024-05-07 09:19:42 -0400

    Updated todo list to use signals

--- packages/web/src/app/components/todo-list/todo-list.component.ts
+++ packages/web/src/app/components/todo-list/todo-list.component.ts
@@ -1,32 +1,25 @@
 import { CdkDragDrop, transferArrayItem, CdkDropListGroup, CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { NgFor } from '@angular/common';
 import { MatList } from '@angular/material/list';
 
 @Component({
-    selector: 'app-todo-list',
-    templateUrl: './todo-list.component.html',
-    styleUrls: ['./todo-list.component.scss'],
-    standalone: true,
-    imports: [
-        CdkDropListGroup,
-        MatList,
-        CdkDropList,
-        NgFor,
-        TodoListItemComponent,
-        CdkDrag,
-    ],
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  standalone: true,
+  imports: [CdkDropListGroup, MatList, CdkDropList, NgFor, TodoListItemComponent, CdkDrag],
 })
 export class TodoListComponent {
-  public todoList: string[] = ['mow the yard', 'take out the trash'];
-  public inProgressList: string[] = ['code todo list for pomodoro'];
-  public doneList: string[] = ['make timer', 'make navigation'];
+  public todoList: WritableSignal<string[]> = signal(['mow the yard', 'take out the trash']);
+  public inProgressList: WritableSignal<string[]> = signal(['code todo list for pomodoro']);
+  public doneList: WritableSignal<string[]> = signal(['make timer', 'make navigation']);
 
-  public dropHandler(droppedItem: CdkDragDrop<string[]>): void {
+  public dropHandler(droppedItem: CdkDragDrop<WritableSignal<string[]>>): void {
     transferArrayItem(
-      droppedItem.previousContainer.data,
-      droppedItem.container.data,
+      droppedItem.previousContainer.data(),
+      droppedItem.container.data(),
       droppedItem.previousIndex,
       droppedItem.currentIndex,
     );
